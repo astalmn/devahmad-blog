1e20fd32c09a883a27b3bc759f6d18af3129b22c
astalmn <a0567202165@gmail.com>
2026-09-25 09:42:00 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
+
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-export default defineConfig({site: process.env.SITE_URL || 'https://devahmad.pages.dev', integrations:[mdx(),sitemap(),tailwind()], output:'static', markdown:{shikiConfig:{theme:'github-dark'}}});
+
+export default defineConfig({
+  site: 'https://devahmad-blog.pages.dev',
+  output: 'static',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
+});
